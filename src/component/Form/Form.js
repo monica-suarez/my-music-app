656247a7794910d0ff98a07f10ca2f78b2467fb3
monica-suarez <shutterbugmo@gmail.com>
2020-10-05 13:02:48 -0500

    added visibility for password and autocomplete

--- src/component/Form/Form.js
+++ src/component/Form/Form.js
@@ -1,19 +1,42 @@
 import React from 'react';
+import clsx from 'clsx';
 import { makeStyles } from '@material-ui/core/styles';
-import {TextField , Button } from '@material-ui/core';
+import {TextField , Button, IconButton, FormControl, InputAdornment, InputLabel, Input } from '@material-ui/core';
+import { Visibility, VisibilityOff } from '@material-ui/icons';
 
 
 const useStyles = makeStyles((theme) => ({
   root: {
     '& .MuiTextField-root': {
         margin: theme.spacing(3),
-        width: '25ch',
+        width: '23ch',
     },
+    textField: {
+        width: '25ch',
+      }
   },
 }));
 
 const LoginForm = () =>{
   const classes = useStyles();
+  const [values, setValues] = React.useState({
+    amount: '',
+    password: '',
+    weight: '',
+    weightRange: '',
+    showPassword: false,
+  });
+  const handleChange = (prop) => (event) => {
+    setValues({ ...values, [prop]: event.target.value });
+  };
+
+  const handleClickShowPassword = () => {
+    setValues({ ...values, showPassword: !values.showPassword });
+  };
+
+  const handleMouseDownPassword = (event) => {
+    event.preventDefault();
+  };
 
   return (
     <form className={classes.root} noValidate autoComplete="off">
@@ -22,10 +45,28 @@ const LoginForm = () =>{
           type="username"
       />
       <br/>
-      <TextField id="standard-password-input"
-          label="Password"
-          type="password"
-          autoComplete="current-password"/>
+           <FormControl className={clsx(classes.margin, classes.textField)}>
+             <InputLabel htmlFor="standard-adornment-password">Password</InputLabel>
+                <Input
+                    id="standard-adornment-password"
+                    type={values.showPassword ? 'text' : 'password'}
+                    value={values.password}
+                    onChange={handleChange('password')}
+                    autoComplete="current-password"
+                    endAdornment={
+                    <InputAdornment position="end">
+                        <IconButton
+                        aria-label="toggle password visibility"
+                        onClick={handleClickShowPassword}
+                        onMouseDown={handleMouseDownPassword}
+                        >
+                        {values.showPassword ? <Visibility /> : <VisibilityOff />}
+                        </IconButton>
+                    </InputAdornment>
+                    }
+                />
+        </FormControl>
+      <br/>
       <br/>
       <Button variant="contained" size="small" color="primary" className={classes.margin} disableElevation>Log In</Button>
       
