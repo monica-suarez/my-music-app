f37c8dcfe59691b2a72da87297c721b1383294c5
monica-suarez <shutterbugmo@gmail.com>
2020-10-11 14:01:23 -0500

    created individual cards and format

--- src/component/Dashboard/Cards/Cards.js
+++ src/component/Dashboard/Cards/Cards.js
@@ -1,57 +1,42 @@
 import React from 'react';
 import { makeStyles, useTheme } from '@material-ui/core/styles';
-import { Card, CardContent, CardActions, Typography }from '@material-ui/core';
+import { Card, CardContent, CardActions, Typography, Box }from '@material-ui/core';
 // import CardMedia from '@material-ui/core/CardMedia';
 // import IconButton from '@material-ui/core/IconButton';
 // import Typography from '@material-ui/core/Typography';
 // import SkipPreviousIcon from '@material-ui/icons/SkipPrevious';
 // import PlayArrowIcon from '@material-ui/icons/PlayArrow';
 // import SkipNextIcon from '@material-ui/icons/SkipNext';
-
-const useStyles = makeStyles((theme) => ({
-    root: {
-      display: 'flex',
-    },
-    details: {
-      display: 'flex',
-      flexDirection: 'column',
-    },
-    content: {
-      flex: '1 0 auto',
-    },
-    cover: {
-      width: 151,
-    },
-    // controls: {
-    //   display: 'flex',
-    //   alignItems: 'center',
-    //   paddingLeft: theme.spacing(1),
-    //   paddingBottom: theme.spacing(1),
-    // },
-    // playIcon: {
-    //   height: 38,
-    //   width: 38,
-    // },
-  }));
+import './cards.css'
 
    const Cards = () =>{
-    const classes = useStyles();
-     const theme = useTheme();
+ 
     return(
-     <Card className={classes.root}>
-         <div className={classes.detail}>
-         <CardContent className={classes.content}>
-          <Typography component="h5" variant="h5">
-            Testing Card
-          </Typography>
-          <Typography variant="subtitle1" color="textSecondary">
-            What in the card is this
-          </Typography>
-        </CardContent>
-         </div>
-         <CardActions>
-         </CardActions>
-     </Card>
+        <Box display="flex" justifyContent="center"  m={1} p={1} bgcolor="background.paper"
+        align-items="center"
+        marginTop="50px">
+            <Box>
+                <Card className="mediaCard" p={1} elevation={3}>
+                    <Typography>
+                        Woot
+                    </Typography>
+                </Card>
+            </Box>
+            <Box>
+                <Card className="mediaCard" p={1} elevation={3}>
+                    <Typography>
+                        Woot
+                    </Typography>
+                </Card>
+            </Box>
+            <Box>
+                <Card className="mediaCard" p={1} elevation={3}>
+                    <Typography>
+                        Woot
+                    </Typography>
+                </Card>
+            </Box>
+        </Box>
     )
    }
 
